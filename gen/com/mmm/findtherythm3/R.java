414d1f4cb449c2942d103082bbbbf7fc59b9e816
akram <20contact12@gmail.com>
2014-02-20 11:35:21 +0100

    fix refs

--- gen/com/mmm/findtherythm3/R.java
+++ gen/com/mmm/findtherythm3/R.java
@@ -27,8 +27,8 @@ public final class R {
         public static final int action_settings=0x7f080004;
         public static final int buttonConf=0x7f080002;
         public static final int buttonQuit=0x7f080003;
-        public static final int buttonStart=0x7f080001;
-        public static final int textViewTitle=0x7f080000;
+        public static final int buttonStart=0x7f080000;
+        public static final int textViewTitle=0x7f080001;
     }
     public static final class layout {
         public static final int activity_drums=0x7f030000;
