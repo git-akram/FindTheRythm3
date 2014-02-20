8321b11f3f43ea8ddd31d84fb771fd03bc9f2c21
medi24 <mehdi-but@hotmail.fr>
2014-02-20 15:01:03 +0100

    ressources2

--- gen/com/mmm/findtherythm3/R.java
+++ gen/com/mmm/findtherythm3/R.java
@@ -23,6 +23,10 @@ public final class R {
     public static final class drawable {
         public static final int background_off=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int leftdown=0x7f020002;
+        public static final int leftup=0x7f020003;
+        public static final int rightdown=0x7f020004;
+        public static final int rightup=0x7f020005;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f090004;
